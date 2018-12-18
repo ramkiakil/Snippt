7ab78a4c3bd648daf883005da6aa77e492f86752
Ramki <rramki51@gmail.com>
2018-12-18 23:26:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Search from './Search';
+import Search from './Search'; //change screen
 import List from './Listing';
 AppRegistry.registerComponent(appName, () => App);
